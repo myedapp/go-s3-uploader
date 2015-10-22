c199030c51208b6bcfca4d00b93f7e4231e9c3ef
Stephen <demontfort.s@gmail.com>
2015-10-22 13:31:35 +1100

    Added decoding for base64 upload streams

--- uploader.go
+++ uploader.go
@@ -2,6 +2,7 @@ package s3uploader
 
 import (
 	"bytes"
+	"encoding/base64"
 	"fmt"
 	"github.com/kr/s3/s3util"
 	"github.com/satori/go.uuid"
@@ -31,6 +32,15 @@ func getFileExtension(file string) string {
  * Upload a file straight to s3 via the multipartreader stream
  */
 func Upload(r *http.Request, settings AwsSettings) (string, string, string, string, error) {
+	var (
+		isBase64     = false
+		base64Prefix = ";base64,"
+		file         string
+		origFile     string
+		ext          string
+		mime         string
+	)
+
 	reader, readerErr := r.MultipartReader()
 	if readerErr != nil {
 		return "", "", "", "", readerErr
@@ -43,23 +53,54 @@ func Upload(r *http.Request, settings AwsSettings) (string, string, string, stri
 			break
 		}
 
-		origFile := part.FileName()
-		// Generate a unique file name
-		ext := getFileExtension(origFile)
-		file := fmt.Sprintf("%s", uuid.NewV4())
-		fullFile := file + "." + ext
-
 		// Check the mime type
 		cBuff := make([]byte, 512)
 		part.Read(cBuff)
 
-		mime := mimemagic.Match("", cBuff)
+		// Check if the file part is base 64 encoded.
+		// If so, strip the type prefix string and re-check
+		// it's mime type.
+		if strings.Contains(string(cBuff), base64Prefix) {
+			isBase64 = true
+
+			s := strings.Split(string(cBuff), base64Prefix)
+
+			// Remove the prefix slice element
+			s = append(s[:0], s[1:]...)
+			sBuff := strings.Join(s, "")
+
+			// Add back to buffer with removed prefix
+			cBuff = []byte(sBuff)
+
+			// base 64 decode
+			decode, _ := base64.StdEncoding.DecodeString(sBuff)
+
+			// Check the mime type
+			mime = mimemagic.Match("", []byte(decode))
+
+			// Cheat for now
+			origFile = "video.mp4"
+		} else {
+			mime = mimemagic.Match("", cBuff)
+			origFile = part.FileName()
+		}
+
+		ext = getFileExtension(origFile)
+
+		// Generate a unique file name
+		file = fmt.Sprintf("%s", uuid.NewV4())
+
 		joinedBody := io.MultiReader(bytes.NewReader(cBuff), part)
+
+		if isBase64 {
+			// base64 decode the stream
+			joinedBody = base64.NewDecoder(base64.StdEncoding, joinedBody)
+		}
 
 		s3util.DefaultConfig.AccessKey = settings.AccessKey
 		s3util.DefaultConfig.SecretKey = settings.SecretKey
 
-		s3File, err := s3util.Create("http://"+settings.Bucket+".s3.amazonaws.com/"+fullFile, nil, nil)
+		s3File, err := s3util.Create(fmt.Sprintf("http://%s.s3.amazonaws.com/%s.%s", settings.Bucket, file, ext), nil, nil)
 		if err != nil {
 			return "", "", "", "", err
 		}
@@ -70,5 +111,5 @@ func Upload(r *http.Request, settings AwsSettings) (string, string, string, stri
 		return file, origFile, ext, mime, nil
 	}
 
-	return "", "", "", "", nil
+	return file, origFile, ext, mime, nil
 }
